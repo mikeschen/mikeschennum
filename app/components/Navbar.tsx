f23e82c5074e4a171bbc6fab6e879f05134dac01
Mike Schennum <mikeschenphoto@gmail.com>
2024-08-14 11:48:29 -0700

    update font size for nav bar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 					<li>
 						<Link
 							href="/"
-							className="rounded-md px-3 py-2 text-sm font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
+							className="rounded-md px-3 py-2 text-md font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
 						>
 							Home
 						</Link>
@@ -16,7 +16,7 @@ const Navbar = () => {
 					<li>
 						<Link
 							href="/resume"
-							className="rounded-md px-3 py-2 text-sm font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
+							className="rounded-md px-3 py-2 text-md font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
 						>
 							Resume
 						</Link>
@@ -24,7 +24,7 @@ const Navbar = () => {
 					<li>
 						<Link
 							href="/contact"
-							className="rounded-md px-3 py-2 text-sm font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
+							className="rounded-md px-3 py-2 text-md font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
 						>
 							Contact
 						</Link>
