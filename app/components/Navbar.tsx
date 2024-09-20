fa9f8983677ee04ac86373f8ee340d322b48f703
Mike Schennum <mikeschenphoto@gmail.com>
2024-09-20 07:17:03 -0700

    remove link for s charts

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -13,14 +13,14 @@ const Navbar = () => {
 							Home
 						</Link>
 					</li>
-					<li>
+					{/* <li>
 						<Link
 							href="https://scharts.vercel.app/"
 							className="rounded-md px-3 py-2 text-md font-medium text-green-500 hover:bg-green-600 hover:text-green-100"
 						>
 							Personal Project: Strava Charts
 						</Link>
-					</li>
+					</li> */}
 					<li>
 						<Link
 							href="/resume"
