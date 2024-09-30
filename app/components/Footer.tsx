68d6d5fbb07df3e5c475bf9c1e410ac9c2554df3
Mike Schennum <mikeschenphoto@gmail.com>
2024-09-30 11:10:57 -0700

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,7 +3,8 @@ const Footer = () => {
 		<footer className="bg-white text-gray-800 py-6">
 			<div className="container mx-auto text-center">
 				<div className="mb-2 text-gray-600 text-xs">
-					Built with Next.js, Tailwind CSS, Open AI and Vercel AI SDK.
+					Built with React, Next.js, Tailwind CSS, Drizzle, Postgres, OpenAI and
+					Vercel AI SDK.
 				</div>
 				<div className="text-gray-600 text-xs">
 					&copy; {new Date().getFullYear()} Michael Schennum. All rights
