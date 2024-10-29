bcacfce25f0e922cd142695bbcdf69eb45af177d
Mike Schennum <mikeschenphoto@gmail.com>
2024-10-29 10:22:28 -0700

    update tsconfig, add types for images

--- app/components/EmailContact.tsx
+++ app/components/EmailContact.tsx
@@ -9,7 +9,7 @@ const EmailContact = () => {
 
 	const [isPending, startTransition] = useTransition();
 
-	const handleSubmitContact = (formData) => {
+	const handleSubmitContact = (formData: FormData) => {
 		startTransition(async () => {
 			const name = formData.get("name") as string;
 			const email = formData.get("email") as string;
