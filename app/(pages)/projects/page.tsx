8197dea06caa48b51faaf4be09267d40e793aceb
Mike Schennum <mikeschenphoto@gmail.com>
2025-03-20 10:14:17 -0700

    add in equal height for projects cards.

--- app/(pages)/projects/page.tsx
+++ app/(pages)/projects/page.tsx
@@ -13,28 +13,28 @@ export default function Projects() {
       title: 'Hangboard Repeaters',
       image: hangBoardRepeatersImage,
       description: 'Android app for rock climbing training',
-			link: "https://play.google.com/store/apps/details?id=com.mikeschen.www.hangboardrepeaters"
+      link: "https://play.google.com/store/apps/details?id=com.mikeschen.www.hangboardrepeaters"
     },
     {
       id: 2,
       title: 'OpenBeta',
       image: openBetaImage,
       description: 'A web app to discover rock climbing routes, photos, and guides',
-			link: "https://openbeta.io"
+      link: "https://openbeta.io"
     },
     {
       id: 3,
       title: 'Scharts',
       image: schartsImage,
       description: 'Visual charts for Strava data',
-			link: "https://scharts.vercel.app"
+      link: "https://scharts.vercel.app"
     },
   ];
 
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-950">
       <Head>
-				<title>Projects</title>
+        <title>Projects</title>
         <meta name="description" content="Michael Schennum's Projects" />
       </Head>
 
@@ -45,25 +45,25 @@ export default function Projects() {
           </h1>
         </div>
 
-        <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3">
           {items.map((item) => (
-						<Link key={item.id} href={item.link}>
-            <div key={item.id} className="group relative bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105">
-              <div className="w-full h-64 bg-gray-200 relative">
-                <Image
-                  src={item.image}
-                  alt={item.title}
-                  layout="fill"
-                  objectFit="cover"
-                  className="group-hover:opacity-90 transition-opacity"
-                />
+            <Link key={item.id} href={item.link}>
+              <div className="group flex flex-col h-[400px] bg-white rounded-lg shadow-md overflow-hidden transition-transform duration-300 hover:scale-105">
+                <div className="w-full h-[250px] bg-gray-200 relative">
+                  <Image
+                    src={item.image}
+                    alt={item.title}
+                    layout="fill"
+                    objectFit="cover"
+                    className="group-hover:opacity-90 transition-opacity"
+                  />
+                </div>
+                <div className="flex-1 p-4">
+                  <h2 className="text-lg font-medium text-gray-900 mb-2">{item.title}</h2>
+                  <p className="text-sm text-gray-500">{item.description}</p>
+                </div>
               </div>
-              <div className="p-4">
-                <h2 className="text-lg font-medium text-gray-900 mb-2">{item.title}</h2>
-                <p className="text-sm text-gray-500">{item.description}</p>
-              </div>
-            </div>
-						</Link>
+            </Link>
           ))}
         </div>
       </main>
